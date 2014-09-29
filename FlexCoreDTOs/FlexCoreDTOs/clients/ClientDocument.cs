c1e33f911ce0d4c4299be189f5c5bb0743d946b3
jloaiza <joseph.lo.cr@gmail.com>
2014-09-28 22:07:04 -0600

    Clientes DTOs

---
+++ FlexCoreDTOs/FlexCoreDTOs/clients/ClientDocument.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexCoreDTOs.clients
+{
+    class ClientDocument
+    {
+        private string _hexBytes;
+        private string _name;
+        private string _description;
+        private int    _clientID;
+
+        public ClientDocument(string pDocHexBytes, string pName, string pDescription, int pClientID)
+        {
+               _hexBytes = pDocHexBytes;
+                   _name = pName;
+            _description = pDescription;
+               _clientID = pClientID;
+        }
+
+        //getters
+        public string getDocHexBytes() { return _hexBytes; }
+
+        public string getName() { return _name; }
+
+        public string getDescription() { return _description; }
+
+        public int getClientID() { return _clientID; }
+
+        //setters
+        public void setDocHexBytes(string pBytes) { _hexBytes = pBytes; }
+
+        public void setName(string pName) { _name = pName; }
+
+        public void setDescription(string pDescription) { _description = pDescription; }
+
+        public void setClientID(int pClientID) { _clientID = pClientID; }
+
+    }
+}
---
+++ FlexCoreDTOs/FlexCoreDTOs/clients/GenericClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlexCoreDTOs.clients
+{
+    public class GenericClientDTO
+    {
+
+        public static readonly string TIPO_JURIDICO = "Jurídico";
+        public static readonly string TIPO_FISICO = "Físico";
+
+        private int     _clientID;
+        private string  _idDoc;
+        private string  _name;
+        private string  _CIF;
+        private bool    _active;
+        private int     _clientTypeID;
+        private bool    _lock;
+
+        public GenericClientDTO(int pIDClient, string pIDDoc, string pName, string pCIF, int pClientTypeID, bool pActive = false, bool pLock = true)
+        {
+                _clientID = pIDClient;
+                   _idDoc = pIDDoc;
+                    _name = pName;
+                     _CIF = pCIF;
+            _clientTypeID = pClientTypeID;
+                  _active = pActive;
+                    _lock = pLock;
+        }
+
+        //Setters
+        public void setIDCliente(int pIDClient) { _clientID = pIDClient; }
+
+        public void setCedula(string pIDDoc) { _idDoc = pIDDoc; }
+
+        public void setNombre(string pName) { _name = pName; }
+
+        public void setCIF(string pCIF) { _CIF = pCIF; }
+
+        public void setIDTipo(int pClientTypeID) { _clientTypeID = pClientTypeID; }
+
+        public void setActivo(bool pActive) { _active = pActive; }
+
+        public void seteClientTypeID(int pClientTypeID) { _clientTypeID = pClientTypeID; }
+
+        public void setLock(bool pLock) { _lock = pLock; }
+
+        //Getters
+        public int getClientID() { return _clientID; }
+
+        public string getIDDoc() { return _idDoc; }
+
+        public string getName() { return _name; }
+
+        public string getCIF() { return _CIF; }
+
+        public bool isActive() { return _active; }
+
+        public int getClientTypeID() { return _clientTypeID; }
+
+        public bool isLock() { return _lock;  }
+
+    }
+}
