394aa4775ef65107917eb080938fe38b940d896e
YeisonCL <yeisoncl94@gmail.com>
2014-10-01 01:31:54 -0600

    :D!

---
+++ ModuloCuentas/DAO/CuentaBeneficiariosDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConexionMySQLServer.ConexionMySql;
+using MySql.Data.MySqlClient;
+
+namespace ModuloCuentas.DAO
+{
+    internal class CuentaBeneficiariosDAO
+    {
+        public static void agregarBeneficiario(string pIdCuenta, string pIdBeneficiario)
+        {
+            String _query = "INSERT INTO CUENTA_BENEFICIARIOS(IDCUENTA, IDBENEFICIARIO) VALUES(@idCuenta, @idBeneficiario);";
+            MySqlConnection _conexionMySQLBase = MySQLManager.nuevaConexion();
+            MySqlCommand _comandoMySQL = _conexionMySQLBase.CreateCommand();
+            _comandoMySQL.CommandText = _query;
+            _comandoMySQL.Parameters.AddWithValue("@idCuenta", pIdCuenta);
+            _comandoMySQL.Parameters.AddWithValue("@idBeneficiario", pIdBeneficiario);
+            _comandoMySQL.ExecuteNonQuery();
+            MySQLManager.cerrarConexion(_conexionMySQLBase);
+        }
+
+        public static void eliminarBeneficiario(string pIdCuenta)
+        {
+            String _query = "DELETE FROM CUENTA_BENEFICIARIOS WHERE idCuenta = @idCuenta";
+            MySqlConnection _conexionMySQLBase = MySQLManager.nuevaConexion();
+            MySqlCommand _comandoMySQL = _conexionMySQLBase.CreateCommand();
+            _comandoMySQL.CommandText = _query;
+            _comandoMySQL.Parameters.AddWithValue("@idCuenta", pIdCuenta);
+            _comandoMySQL.ExecuteNonQuery();
+            MySQLManager.cerrarConexion(_conexionMySQLBase);
+        }
+    }
+}
---
+++ ModuloCuentas/Generales/Transformaciones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCuentas.Generales
+{
+    internal static class Transformaciones
+    {
+        public static int boolToInt(bool pEstado)
+        {
+            if(pEstado == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool intToBool(int pEstado)
+        {
+            if (pEstado == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
