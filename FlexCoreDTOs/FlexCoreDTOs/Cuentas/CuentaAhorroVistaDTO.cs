de7d794357483dab4f8bef5dac5355bce88c6cde
YeisonCL <yeisoncl94@gmail.com>
2014-10-01 02:03:50 -0600

    Agregados los DTO de cuentas.

---
+++ FlexCoreDTOs/FlexCoreDTOs/Cuentas/CuentaAhorroVistaDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexCoreDTOs.Cuentas
+{
+    public class CuentaAhorroVistaDTO : CuentaAhorroDTO
+    {
+        //RECORDAR QUE TAMBIEN VOY A RECIBIR UN CLIENTE DUEÑO DE LA CUENTA Y UNA LISTA DE CLIENTES DTO COMO BENEFICIARIOS.
+        //HAY QUE MODIFICAR LO NECESARIO DONDE CORRESPONDE 
+
+        private decimal _saldoFlotante;
+       
+        public decimal getSaldoFlotante()
+        {
+            return _saldoFlotante;
+        }
+
+        public void setSaldoFlotante(decimal pSaldoFlotante)
+        {
+            _saldoFlotante = pSaldoFlotante;
+        }
+    }
+}
