6bc44c57bc3014536d2e4d3e03d2fcbc2e2e0b4d
jloaiza <joseph.lo.cr@gmail.com>
2014-10-06 00:23:40 -0600

    arreglo en namespace de administration

--- FlexCoreDTOs/FlexCoreDTOs/administration/TransaccionesVueloDTO.cs
+++ FlexCoreDTOs/FlexCoreDTOs/administration/TransaccionesVueloDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlexCore.administration
+namespace FlexCoreDTOs.administration
 {
     public class TransaccionesVueloDTO
     {
