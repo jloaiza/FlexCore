5130a78af09fddf9daa0f29bc26d6b088d3c4d0e
jloaiza <joseph.lo.cr@gmail.com>
2014-10-06 01:25:04 -0600

    -.-

---
+++ FlexCoreDAOs/Pruebas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlexCoreDAOs.clients;
+using FlexCoreDTOs.clients;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PersonDAO personDAO = new PersonDAO();
+            //PersonDTO person = new PersonDTO(DTOConstants.DEFAULT_INT_ID, "%Joseph%", "", "");
+            //personDAO.insert(person);
+
+            //ClientDAO cd = new ClientDAO();
+            //ClientDTO c = new ClientDTO(1, "cif1", true);
+            //cd.getAll();
+
+            JuridicalClientViewDAO jcv = new JuridicalClientViewDAO();
+            jcv.getAll();
+
+            //List<PersonDTO> results = personDAO.search(person);
+            //foreach (PersonDTO p in results)
+            //{
+            //    Console.WriteLine(p.getPersonID());
+            //    Console.WriteLine(p.getName());
+            //    Console.WriteLine(p.getPersonType());
+            //    Console.WriteLine("-----------------");
+            //}
+            Console.ReadLine();         
+        }
+    }
+}
