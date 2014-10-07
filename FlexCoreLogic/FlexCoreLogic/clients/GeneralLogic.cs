5815a3e9a951432caba371b1e7aed56232815113
jloaiza <joseph.lo.cr@gmail.com>
2014-10-06 20:15:19 -0600

    Inicio de trabajo en la logica

---
+++ FlexCoreLogic/FlexCoreLogic/clients/GeneralLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexCoreLogic.clients
+{
+    class GeneralLogic<T>
+    {
+        public abstract void insert(T pObject);
+        public abstract void delete(T pObject);
+        public abstract void update(T pNewObject, T pLastObject);
+        public abstract List<T> search(T pObject);
+        public abstract List<T> getAll();
+    }
+}
