ba8ca21b6254d99381bb974ce9e864b6de2e0bad
YeisonCL <yeisoncl94@gmail.com>
2014-10-07 20:19:12 -0600

    Agregado DAO de cuentas.

---
+++ FlexCoreDAOs/FlexCoreDAOs/cuentas/CuentaBeneficiariosDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConexionMySQLServer.ConexionMySql;
+using MySql.Data.MySqlClient;
+using FlexCoreDTOs.clients;
+using FlexCoreDTOs.cuentas;
+
+namespace ModuloCuentas.DAO
+{
+    internal class CuentaBeneficiariosDAO
+    {
+        public static void agregarBeneficiarios(CuentaAhorroVistaDTO pCuentaAhorroVista, MySqlCommand pComando)
+        {
+            int _id = CuentaAhorroDAO.obtenerCuentaAhorroID(pCuentaAhorroVista, pComando);
+            foreach (PhysicalPersonDTO beneficiario in pCuentaAhorroVista.getListaBeneficiarios())
+            {
+                String _query = "INSERT INTO CUENTA_BENEFICIARIOS(IDCUENTA, IDBENEFICIARIO) VALUES(@idCuenta, @idBeneficiario);";
+                pComando.Parameters.Clear();
+                pComando.CommandText = _query;
+                pComando.Parameters.AddWithValue("@idCuenta", _id);
+                pComando.Parameters.AddWithValue("@idBeneficiario", beneficiario.getPersonID());
+                pComando.ExecuteNonQuery();
+            }
+        }
+
+        public static void eliminarBeneficiario(CuentaAhorroVistaDTO pCuentaAhorroVista, MySqlCommand pComando)
+        {
+            int _id = CuentaAhorroDAO.obtenerCuentaAhorroID(pCuentaAhorroVista, pComando);
+            String _query = "DELETE FROM CUENTA_BENEFICIARIOS WHERE idCuenta = @idCuenta";
+            pComando.Parameters.Clear();
+            pComando.CommandText = _query;
+            pComando.Parameters.AddWithValue("@idCuenta", _id);
+            pComando.ExecuteNonQuery();
+        }
+
+        public static List<PhysicalPersonDTO> obtenerListaBeneficiarios(CuentaAhorroVistaDTO pCuentaAhorroVista, MySqlCommand pComando)
+        {
+            int _id = CuentaAhorroDAO.obtenerCuentaAhorroID(pCuentaAhorroVista, pComando);
+            List<PhysicalPersonDTO> _listaBeneficiarios = new List<PhysicalPersonDTO>();
+            String _query = "SELECT * FROM CUENTA_BENEFICIARIOS WHERE IDCUENTA = @idCuenta;";
+            pComando.Parameters.Clear();
+            pComando.CommandText = _query;
+            pComando.Parameters.AddWithValue("@idCuenta", _id);
+            MySqlDataReader _reader = pComando.ExecuteReader();
+            if (_reader.Read())
+            {
+                PhysicalPersonDTO _beneficiario = new PhysicalPersonDTO(Convert.ToInt32(_reader["idBeneficiario"]), "", "", "", "");
+                _listaBeneficiarios.Add(_beneficiario);
+            }
+            _reader.Close();
+            return _listaBeneficiarios;
+        }
+    }
+}
---
+++ FlexCoreDAOs/FlexCoreDAOs/cuentas/PeriocidadAhorroDAO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ConexionMySQLServer.ConexionMySql;
+
+namespace ModuloCuentas.DAO
+{
+    internal class PeriocidadAhorroDAO
+    {
+        public static int agregarMagnitudPeriocidad(int pMagnitud, int pTipoPeriodo, MySqlCommand pComando)
+        {
+            String _query = "INSERT INTO PERIODICIDAD_AHORRO(MAGNITUD, TIPO) VALUES(@magnitud, @tipo);";
+            pComando.CommandText = _query;
+            pComando.Parameters.Clear();
+            pComando.Parameters.AddWithValue("@magnitud", pMagnitud);
+            pComando.Parameters.AddWithValue("@tipo", pTipoPeriodo);
+            pComando.ExecuteNonQuery();
+            int _lastId = Convert.ToInt32(pComando.LastInsertedId);
+            return _lastId;
+        }
+
+        public static void eliminarPeriodicidadAhorro(int pIdPeriodicidad, MySqlCommand pComando)
+        {
+            String _query = "DELETE FROM PERIODICIDAD_AHORRO WHERE idPeriodo = @idPeriodo;";
+            pComando.CommandText = _query;
+            pComando.Parameters.Clear();
+            pComando.Parameters.AddWithValue("@idPeriodo", pIdPeriodicidad);
+            pComando.ExecuteNonQuery();
+        }
+
+        public static int obtenerIdPeriodo(string pNumeroCuenta, MySqlCommand pComando)
+        {
+            int _idPeriodo = 0;
+            String _query = "SELECT * FROM CUENTA_AHORRO_AUTOMATICO_V WHERE numCuenta = @numCuenta;";
+            pComando.CommandText = _query;
+            pComando.Parameters.Clear();
+            pComando.Parameters.AddWithValue("@numCuenta", pNumeroCuenta);
+            MySqlDataReader _reader = pComando.ExecuteReader();
+            if(_reader.Read())
+            {
+                _idPeriodo = Convert.ToInt32(_reader["idPeriodo"]);
+            }
+            _reader.Close();
+            return _idPeriodo;
+        }
+
+        public static void modificarPeriodicidadAhorro(int pIdPeriodicidad, int pMagnitud, int pTipo, MySqlCommand pComando)
+        {
+            String _query = "UPDATE PERIODICIDAD_AHORRO SET MAGNITUD = @magnitud, TIPO = @tipo WHERE IDPERIODO = @idPeriodicidad;";
+            pComando.CommandText = _query;
+            pComando.Parameters.Clear();
+            pComando.Parameters.AddWithValue("@magnitud", pMagnitud);
+            pComando.Parameters.AddWithValue("@tipo", pTipo);
+            pComando.Parameters.AddWithValue("@idPeriodicidad", pIdPeriodicidad);
+            pComando.ExecuteNonQuery();
+        }
+    }
+}
