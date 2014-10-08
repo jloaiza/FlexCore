b41bb6711c1253b0c7402cad08c5f5a9e23daa08
YeisonCL <yeisoncl94@gmail.com>
2014-10-07 20:34:47 -0600

    :D!!

--- ModuloCuentas/Managers/CuentaAhorroAutomaticoManager.cs
+++ ModuloCuentas/Managers/CuentaAhorroAutomaticoManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ModuloCuentas.DAO;
 using ModuloCuentas.Generales;
 using System.Threading;
 using FlexCoreDTOs.cuentas;
--- ModuloCuentas/Managers/CuentaAhorroVistaManager.cs
+++ ModuloCuentas/Managers/CuentaAhorroVistaManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ModuloCuentas.DAO;
 using System.Threading;
 using FlexCoreDTOs.cuentas;
 using ModuloCuentas.Generales;
