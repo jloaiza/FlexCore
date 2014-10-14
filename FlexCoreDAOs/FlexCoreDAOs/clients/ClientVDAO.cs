07816ff6c8cfc1bc86a4a2f7b0d3da528be6d4c5
jloaiza <joseph.lo.cr@gmail.com>
2014-10-14 01:31:34 -0600

    ClientVDAO

---
+++ FlexCoreDAOs/FlexCoreDAOs/clients/ClientVDAO.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using ConexionMySQLServer.ConexionMySql;
+using MySql.Data.MySqlClient;
+using FlexCoreDTOs.clients;
+
+namespace FlexCoreDAOs.clients
+{
+    class ClientVDAO:GeneralDAO<ClientVDTO>
+    {
+        public static readonly string JURIDICAL_PERSON = "Juridica";
+        public static readonly string PHYSICAL_PERSON = "Fisica";
+
+        public static readonly string CLIENT_ID = "idCliente";
+        public static readonly string CIF = "CIF";
+        public static readonly string ACTIVE = "activo";
+        public static readonly string NAME = "nombre";
+        public static readonly string ID_CARD = "cedula";
+        public static readonly string TYPE = "tipo";
+
+        protected override string getFindCondition(ClientVDTO pClient)
+        {
+            string condition = "";
+            if (pClient.getClientID() != DTOConstants.DEFAULT_INT_ID)
+            {
+                condition = addCondition(condition, String.Format("{0}= @{0}", CLIENT_ID));
+            }
+            if (pClient.getCIF() != DTOConstants.DEFAULT_STRING)
+            {
+                condition = addCondition(condition, String.Format("{0} =@{0}", CIF));
+            }
+            if (pClient.getName() != DTOConstants.DEFAULT_STRING)
+            {
+                condition = addCondition(condition, String.Format("{0} LIKE @{0}", NAME));
+            }
+            if (pClient.getIDCard() != DTOConstants.DEFAULT_STRING)
+            {
+                condition = addCondition(condition, String.Format("{0}= @{0}", ID_CARD));
+            }
+            if (pClient.getPersonType() != DTOConstants.DEFAULT_STRING)
+            {
+                condition = addCondition(condition, String.Format("{0}= @{0}", TYPE));
+            }
+            return condition;
+        }
+
+        protected override void setFindParameters(MySqlCommand pCommand, ClientVDTO pClient)
+        {
+            if (pClient.getClientID() != DTOConstants.DEFAULT_INT_ID)
+            {
+                pCommand.Parameters.AddWithValue("@" + CLIENT_ID, pClient.getClientID());
+            }
+            if (pClient.getCIF() != DTOConstants.DEFAULT_STRING)
+            {
+                pCommand.Parameters.AddWithValue("@" + CIF, pClient.getCIF());
+            }
+            if (pClient.getName() != DTOConstants.DEFAULT_STRING)
+            {
+                pCommand.Parameters.AddWithValue("@" + NAME, pClient.getName());
+            }
+            if (pClient.getIDCard() != DTOConstants.DEFAULT_STRING)
+            {
+                pCommand.Parameters.AddWithValue("@" + ID_CARD, pClient.getIDCard());
+            }
+            if (pClient.getPersonType() != DTOConstants.DEFAULT_STRING)
+            {
+                pCommand.Parameters.AddWithValue("@" + TYPE, pClient.getPersonType());
+            }
+        }
+
+        public override List<ClientVDTO> search(ClientVDTO pClient, MySqlCommand pCommand, int pPageNumber = 0, int pShowCount = 0, params string[] pOrderBy)
+        {
+            pCommand.Parameters.Clear();
+            string selection = "*";
+            string from = "CLIENTE_V";
+            string condition = getFindCondition(pClient);
+            string query = getSelectQuery(selection, from, condition, pPageNumber, pShowCount, pOrderBy);
+
+            pCommand.CommandText = query;
+            setFindParameters(pCommand, pClient);
+
+            MySqlDataReader reader = pCommand.ExecuteReader();
+            List<ClientVDTO> list = new List<ClientVDTO>();
+
+            while (reader.Read())
+            {
+                ClientVDTO client = new ClientVDTO();
+                client.setClientID((int)reader[CLIENT_ID]);
+                client.setCIF(reader[CIF].ToString());
+                client.setActive(sqlToBool((int)reader[ACTIVE]));
+                client.setName(reader[NAME].ToString());
+                client.setIDCard(reader[ID_CARD].ToString());
+                client.setPersonType(reader[TYPE].ToString());
+                list.Add(client);
+            }
+            return list;
+        }
+
+        public override List<ClientVDTO> getAll(MySqlCommand pCommand, int pPageNumber, int pShowCount, params string[] pOrderBy)
+        {
+            pCommand.Parameters.Clear();
+            string query = getSelectQuery("*", "CLIENTE_V", pPageNumber, pShowCount, pOrderBy);
+            pCommand.CommandText = query;
+            MySqlDataReader reader = pCommand.ExecuteReader();
+            List<ClientVDTO> list = new List<ClientVDTO>();
+            while (reader.Read())
+            {
+                ClientVDTO client = new ClientVDTO();
+                client.setClientID((int)reader[CLIENT_ID]);
+                client.setCIF(reader[CIF].ToString());
+                client.setActive(sqlToBool((int)reader[ACTIVE]));
+                client.setName(reader[NAME].ToString());
+                client.setIDCard(reader[ID_CARD].ToString());
+                client.setPersonType(reader[TYPE].ToString());
+                list.Add(client);
+            }
+            return list;
+        }
+    }
+}
