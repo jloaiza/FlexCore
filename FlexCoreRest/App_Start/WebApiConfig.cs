9ac663512227f6acf0f06e5bb457838aa6b42d14
YeisonCL <yeisoncl94@gmail.com>
2014-10-14 22:36:53 -0600

    Creado servicio rest.

---
+++ FlexCoreRest/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace FlexCoreRest
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "Pagos",
+                routeTemplate: "pagar",
+                defaults: new { controller = "pagos" }
+            );
+        }
+    }
+}
---
+++ FlexCoreRest/Controllers/PagosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FlexCoreRest.Controllers
+{
+    public class PagosController : ApiController
+    {
+        //POST /pagar
+        //Reliza un nuevo pago
+        public HttpResponseMessage PostPagar()
+        {
+            string _datosPost = Request.Content.ReadAsStringAsync().Result;
+            HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, _datosPost);
+            _request.Headers.Add("Access-Control-Allow-Origin", "*");
+            return _request;
+        }
+    }
+}
