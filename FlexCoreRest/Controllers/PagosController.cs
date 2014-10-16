a06c5e639456dc19a6cc8ce74048c585a4a61a82
YeisonCL <yeisoncl94@gmail.com>
2014-10-16 01:21:40 -0600

    Terminado el restfull.

--- FlexCoreRest/Controllers/PagosController.cs
+++ FlexCoreRest/Controllers/PagosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlexCoreLogic.pagos.VerificacionPreviaPagos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,10 +14,24 @@ namespace FlexCoreRest.Controllers
         //Reliza un nuevo pago
         public HttpResponseMessage PostPagar()
         {
+            PrePagos _iniciarPago = new PrePagos();
             string _datosPost = Request.Content.ReadAsStringAsync().Result;
-            HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, _datosPost);
+            List<string> _valores = new List<string>();
+            _valores = crearListaValores(_datosPost);
+            HttpResponseMessage _request = Request.CreateResponse(HttpStatusCode.OK, _iniciarPago.iniciarPago(_valores[1], _valores[3], _valores[5], Convert.ToDecimal(_valores[7])));
             _request.Headers.Add("Access-Control-Allow-Origin", "*");
             return _request;
+        }
+
+        private List<string> crearListaValores(string pPost)
+        {
+            List<string> _valores = new List<string>();
+            string[] _arregloValores = pPost.Split(new string[] { "=", "&" }, StringSplitOptions.None);
+            foreach (string valor in _arregloValores)
+            {
+                _valores.Add(valor);
+            }
+            return _valores;
         }
     }
 }
